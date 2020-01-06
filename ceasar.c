23da2057011effdac0c8e73daf65e955d35c3b2e
Christ-Yan Love Larose <55823603+cyll99@users.noreply.github.com>
2020-01-05 20:05:32 -0600

    Create ceasar.c

---
+++ ceasar.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+void min(int a);
+int main(int argc, char *argv[])
+{
+
+    if (argc == 2)
+    {
+        int k = atoi(argv[1]);
+        signed char c[265];
+        int *text = NULL;
+        printf("Plaintext:");
+        scanf("%[^\n]s", c);
+        
+        int e = strlen(c);
+        text = malloc(sizeof(int)*e);
+        if(text == NULL)
+            exit(1);
+
+        printf("ciphertext: ");
+        for (int i = 0; i < e; i++)
+        {
+            int l = c[i] + k;
+            if (c[i] > 64 && c[i] < 91)
+            {
+
+                if (l > 90)
+                {
+                    while (l > 90)
+                    {
+                        l = l - 90 + 64;
+                    }
+
+                    printf("%c", l);
+                }
+                else
+                {
+                    printf("%c", l);
+                }
+            }
+            else if (c[i] > 96 && c[i] < 123)
+            {
+                if (l > 122)
+                {
+                    while (l > 122)
+                    {
+                        l = l - 122 + 96;
+                    }
+
+                    printf("%c", l);
+                }
+                else
+                {
+                    printf("%c", l);
+                }
+            }
+            else
+            {
+                printf("%c", c[i]);
+            }
+        }
+        printf("\n");
+        return 0;
+        free(text);
+    }
+
+    else
+    {
+        printf("Usage: ./caesar key \n");
+        return 1;
+    }
+}
